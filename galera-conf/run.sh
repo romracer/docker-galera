d28d54b9c5dc08a6cc85325bd600f304925d23ff
Scott <romracer@gmail.com>
2017-02-24 12:34:18 -0600

    Workaround alpine 3.4 bug

--- galera-conf/run.sh
+++ galera-conf/run.sh
@@ -3,4 +3,4 @@
 cp /start_galera.sh /giddyup /opt/rancher/
 /giddyup service wait scale
 
-exec /confd --backend=rancher --prefix=/2015-07-25
+exec /confd --backend=rancher --prefix=/2015-07-25 --node=rancher-metadata.rancher.internal
